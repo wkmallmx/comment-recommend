f0cd2ff509f7f425a3b8727cde2b799dccaad981
wzy <848577923@qq.com>
2024-03-13 15:12:31 +0800

    Delete axios.ts and update map.tsx

--- open-react-template/components/map.tsx
+++ open-react-template/components/map.tsx
@@ -1,62 +1,67 @@
-'use client'
+"use client";
 
-import React, {useState} from 'react';
-import {Map, APILoader, Provider, Marker, InfoWindow} from '@uiw/react-baidu-map';
-
+import React, { useState } from "react";
+import {
+  Map,
+  APILoader,
+  Provider,
+  Marker,
+  InfoWindow,
+} from "@uiw/react-baidu-map";
 const BaiduMap = () => {
+  const center = { lng: -73.754968, lat: 42.6511674 };
 
-    const center = {lng: -73.754968, lat: 42.6511674}
+  const icon = new BMap.Symbol(BMap_Symbol_SHAPE_POINT, {
+    scale: 2, // 图标缩放大小
+    fillColor: "red", // 填充颜色
+    fillOpacity: 0.8, // 填充透明度
+  });
 
-    const icon = new BMap.Symbol(BMap_Symbol_SHAPE_POINT, {
-        scale: 2, // 图标缩放大小
-        fillColor: "red", // 填充颜色
-        fillOpacity: 0.8, // 填充透明度
-    })
+  const [isOpen, setIsOpen] = useState(false);
+  const [content, setContent] = useState("这是信息");
 
-    const [isOpen, setIsOpen] = useState(false)
-    const [content, setContent] = useState('这是信息')
-
-    function markerRef(props: any) {
-        if (props && props.marker) {
-            console.log('marker::', props.marker, props.map, props.BMap);
-        }
+  function markerRef(props: any) {
+    if (props && props.marker) {
+      console.log("marker::", props.marker, props.map, props.BMap);
     }
+  }
 
-    function infoWindowRef(props: any) {
-        if (props && props.infoWindow) {
-            console.log('infoWindow:', props.infoWindow, props.map, props.BMap);
-        }
+  function infoWindowRef(props: any) {
+    if (props && props.infoWindow) {
+      console.log("infoWindow:", props.infoWindow, props.map, props.BMap);
     }
+  }
 
-    return (
+  return (
+    <Map zoom={13} center={center} enableScrollWheelZoom={true}>
+      {/* 标记 */}
+      <Marker
+        ref={markerRef}
+        position={{ lng: -73.754968, lat: 42.6511674 }}
+        icon={icon}
+        onClick={() => setIsOpen(!isOpen)}
+      />
 
-        <Map zoom={13} center={center} enableScrollWheelZoom={true}>
+      {/* 条件渲染信息窗口 */}
+      <InfoWindow
+        ref={infoWindowRef}
+        position={center}
+        content="<p style='font-size: 16px'>信息</p>"
+        isOpen={isOpen}
+      />
+    </Map>
+  );
+};
 
-            {/* 标记 */}
-            <Marker ref={markerRef} position={{lng: -73.754968, lat: 42.6511674}} icon={icon}
-                    onClick={() => setIsOpen(!isOpen)}/>
-
-            {/* 条件渲染信息窗口 */}
-            <InfoWindow ref={infoWindowRef} position={center} content="<p style='font-size: 16px'>信息</p>" isOpen={isOpen}/>
-
-        </Map>
-    )
-}
 
 export default function MapComponent() {
-    return (
-        <div style={{width: 1200, height: 800}}>
-            <APILoader akay="WDDVx86wFkDa8vMCDReijDGFIB4EWLxi">
-                <Provider>
-                    <BaiduMap/>
-                </Provider>
-            </APILoader>
-        </div>
-    )
+  return (
+    <div style={{ width: 1200, height: 800 }}>
+      <APILoader akay="WDDVx86wFkDa8vMCDReijDGFIB4EWLxi">
+        <Provider>
+          <BaiduMap />
+        </Provider>
+      </APILoader>
+    </div>
+  );
 }
-
-
-
-
-
-
